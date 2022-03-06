58f6156f403c4e6d595bcf167c285a41c7b7c075
waldigonzalez <gonzalezsuse92gmail.com>
2022-03-06 17:06:11 -0600

    actividad

---
+++ ejercicio3.v3.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <?php require_once "menu.php" ?>
+    <title>Estructuras de Control</title>
+  </head>
+  <body>
+  <div class="container"><br>
+    <div class="row justify-content-center">
+        <div class="col-6 p-5 bg-white shadow-lg rounded">
+        <h3>Estructura ELSEIF</h3>
+            <hr>
+            <?php 
+            $mensaje ="Hola" ;
+            
+            if ($mensaje=="Hola")
+            echo "Muy buenas, usuario";
+             elseif ($mensaje=="Adios"){
+                echo "Hasta pronto";
+           
+            }
+            else {
+                echo ".......";
+            }
+            
+
+           
+            
+            ?>
+
+        </div>
+    </div>
+  </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ menu.php
@@ -0,0 +1,34 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Tecnologia Web</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="index.php">Inicio</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Estructuras de Control
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="ejercicio1.php">EJERCICIO1</a></li>
+            <li><a class="dropdown-item" href="ejercicio2.php">EJERCICIO2</a></li>
+            <li><a class="dropdown-item" href="ejercicio3.php">EJERCICIO3</a></li>
+            <li><a class="dropdown-item" href="ejercicio3v3.php">EJERCICIO3.v2</a></li>
+            <li><a class="dropdown-item" href="ejercicio4.php">EJERCICIO4</a></li>
+            <li><a class="dropdown-item" href="ejercicio4.2.php">EJERCICIO4.2</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
